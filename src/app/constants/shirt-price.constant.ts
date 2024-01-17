f25288c4ba0dc46ea35c3cee999baf2dafdc7cff
Mack Chen <wind83410@gmail.com>
2024-01-18 01:44:08 +0800

    fix: 補回 XS 尺寸選項

--- src/app/constants/shirt-price.constant.ts
+++ src/app/constants/shirt-price.constant.ts
@@ -1,4 +1,5 @@
 export const SHIRT_SIZE_PRICES = {
+  XS: 1100,
   S: 1100,
   M: 1100,
   L: 1100,
--- src/app/models/shirt.model.ts
+++ src/app/models/shirt.model.ts
@@ -1,4 +1,4 @@
-export type ShirtSizes = 'S' | 'M' | 'L' | 'XL' | '2XL' | '3XL';
+export type ShirtSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | '2XL' | '3XL';
 
 export interface ShirtPurchase {
   sizePrice: number,
